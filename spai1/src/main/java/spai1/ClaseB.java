ba88b4eff82a6b61365b611677b17d92dd16b243
Jorge <sirjorgete@gmail.com>
2015-09-26 10:33:46 +0200

    Ready to release

--- spai1/src/main/java/spai1/ClaseB.java
+++ spai1/src/main/java/spai1/ClaseB.java
@@ -13,6 +13,14 @@ public class ClaseB {
 		return "ClaseB [name=" + name + ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()="
 				+ super.toString() + "]";
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 	
 	
 }
